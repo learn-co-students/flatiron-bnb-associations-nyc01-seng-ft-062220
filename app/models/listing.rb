0822dc1223d2433593d1e392ec20f440e6511d53
Dmitry Sychev <axiumone@gmail.com>
2020-07-20 19:48:52 -0400

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,11 +1,11 @@
 class Listing < ApplicationRecord
+  belongs_to :host, :class_name => :User
   belongs_to :neighborhood
-  belongs_to :host, :class_name => "User"
   has_many :reservations
+  has_many :reviews, through: :reservations
   has_many :guests, through: :reservations
   # has_many :guests, :foreign_key => "guest_id"
-  has_many :reviews, through: :reservations
-
+ 
   def guests
     self.reservations.map { |reservation| reservation.guest }
   end
